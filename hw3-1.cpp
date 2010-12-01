6c85859d5da23df67ebd9a7332446b2701953b89
Karl Diao <klandor@gmail.com>
2010-12-01 13:12:04 +0800

    add html tags.

--- hw3-1.cpp
+++ hw3-1.cpp
@@ -3,10 +3,13 @@
 using namespace std;
 int main (int argc, char * const argv[]) {
 	cout << "Content-type: text/html\n\n";
+	cout << "<html><head></head><body>"
 	string s1,s2;
 	while( getline(cin, s1, '=') ){
 		getline(cin, s2, '&');
-		cout << s1 << '=' << s2 << endl;
+		cout << s1 << '=' << s2 << "<BR>" <<endl;
 	}
+	
+	cout << "</body></html>";
 	return 0;
 }
